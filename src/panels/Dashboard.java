d2a2a2cfe416bffdd985f9308bdaa54ea1ec85d9
Angelo Gabriel Bautista <s1010036@usls.edu.ph>
2026-04-06 15:25:50 +0800

    dashboard update: added bank and ewallet selection

--- src/panels/Dashboard.java
+++ src/panels/Dashboard.java
@@ -3,6 +3,8 @@ package panels;
 import javax.swing.*;
 import javax.xml.crypto.Data;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Dashboard panel showing summary/statistics.
@@ -16,7 +18,7 @@ import java.awt.*;
  * - Add a refresh button to update the stats -done
  * - Make it visually appealing (use colors, larger fonts, icons)
  */
-public class Dashboard extends JPanel {
+public class Dashboard extends JPanel implements ActionListener {
     private JLabel countLabel;
     private JLabel average;
 
@@ -34,7 +36,55 @@ public class Dashboard extends JPanel {
         centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
         centerPanel.setBorder(BorderFactory.createEmptyBorder(20, 40, 10, 40));
 
+        // Left Content
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel,BoxLayout.Y_AXIS));
+        leftPanel.setBorder(BorderFactory.createEmptyBorder(20,40,10,40));
 
+        //Combo box para sa banks
+        String[] BankNames = {
+                "BDO Unibank",
+                "Bank of the Philippine Islands (BPI)",
+                "Land Bank of the Philippines",
+                "Metropolitan Bank & Trust Company (Metrobank)",
+                "Philippine National Bank (PNB)",
+                "Security Bank",
+                "China Banking Corporation (China Bank)",
+                "Rizal Commercial Banking Corporation (RCBC)",
+                "Union Bank of the Philippines (UnionBank)",
+                "Development Bank of the Philippines (DBP)",
+                "EastWest Bank",
+                "Asia United Bank (AUB)",
+                "Philippine Bank of Communications (PBCom)",
+                "Philippine Veterans Bank",
+                "Philtrust Bank",
+                "Bank of Commerce"
+        };
+
+        JComboBox BankList = new JComboBox(BankNames);
+        BankList.setMaximumSize(new Dimension(250, 30));
+        BankList.setPreferredSize(new Dimension(250, 30));
+        BankList.setSelectedIndex(0);
+        BankList.addActionListener(this);
+
+        //combo box para sa e wallets
+        String[] EwalletNames = {
+                "GCash",
+                "Maya",
+                "GoTyme",
+                "Tonik",
+                "UNO Digital Bank",
+                "UnionDigital Bank",
+                "GrabPay",
+                "ShopeePay"
+        };
+        JComboBox EwalletList = new JComboBox(EwalletNames);
+        EwalletList.setMaximumSize(new Dimension(250, 30));
+        EwalletList.setPreferredSize(new Dimension(250, 30));
+        EwalletList.setSelectedIndex(0);
+        EwalletList.addActionListener( this);
+        leftPanel.add(EwalletList);
+        leftPanel.add(BankList);
         centerPanel.add(Box.createVerticalStrut(20));
 
         //Button
@@ -46,9 +96,15 @@ public class Dashboard extends JPanel {
 
 
         add(centerPanel, BorderLayout.CENTER);
+        add(leftPanel, BorderLayout.WEST);
     }
 
     private void refreshData() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
