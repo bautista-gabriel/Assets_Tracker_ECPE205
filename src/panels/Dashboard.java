70521edea48d48dfe8a6dd8134be59d22dfee78d
Angelo Gabriel Bautista <s1010036@usls.edu.ph>
2026-04-08 10:37:02 +0800

    ..

--- src/panels/Dashboard.java
+++ src/panels/Dashboard.java
@@ -16,56 +16,64 @@ public class Dashboard extends JPanel {
     public Dashboard() {
         setLayout(new BorderLayout());
 
-        // Welcome Message
-        JLabel title = new JLabel("Dashboard", SwingConstants.CENTER);
-        title.setFont(new Font("Times New Roman", Font.BOLD, 28));
-        title.setBorder(BorderFactory.createEmptyBorder(20, 0, 10, 0));
-        add(title, BorderLayout.NORTH);
-
         // Center content
         JPanel centerPanel = new JPanel();
         centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
         centerPanel.setBorder(BorderFactory.createEmptyBorder(20, 40, 10, 40));
 
-        totalLabel = new JLabel("Total Assets: ₱0.00");
+        totalLabel = new JLabel("Welcome Juan Dela Cruz");
         totalLabel.setFont(new Font("Arial", Font.BOLD, 20));
-        add(totalLabel, BorderLayout.NORTH);
+        totalLabel.setBounds(1,1,1,1);
+        centerPanel.add(totalLabel,BorderLayout.NORTH);
 
-        String[] columns = {"Provider", "Type", "Amount"};
-        tableModel = new DefaultTableModel(columns, 0);
-        assetsTable = new JTable(tableModel);
+        JLabel banks = new JLabel("Banks");
+        banks.setFont(new Font("Arial", Font.BOLD, 14));
+        banks.setBounds(1,10,1,1);
+        centerPanel.add(banks,BorderLayout.NORTH);
 
-        JScrollPane scrollPane = new JScrollPane(assetsTable);
-        add(scrollPane, BorderLayout.CENTER);
+        JLabel ewallets = new JLabel("E-Wallets");
 
-            updateDashboard();
-    }
+        add(centerPanel,BorderLayout.CENTER);
 
-    public void updateDashboard() {
-        tableModel.setRowCount(0);
-        String sql = "SELECT provider_name, SUM(amount) AS total_amount from assets GROUP BY provider_name ORDER BY provider_name";
 
-        double totalAssets = 0.0;
-        try (Connection conn = DatabaseConnection.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql);
-             ResultSet rs = pstmt.executeQuery()) {
-            while (rs.next()) {
-                String provider = rs.getString("provider_name");
-                double amount = rs.getDouble("total_amount");
-                String type;
 
-                if (provider.equals("GCash") || provider.equals("Maya") || provider.equals("GrabPay") || provider.equals("ShopeePay")) {
-                    type = "E-Wallet";
-                } else {
-                    type = "Bank";
-                }
-                tableModel.addRow(new Object[]{provider, type, String.format("₱%.2f", amount)});
-                totalAssets += amount;
+//        String[] columns = {"Provider", "Type", "Amount"};
+//        tableModel = new DefaultTableModel(columns, 0);
+//        assetsTable = new JTable(tableModel);
+//
+//        JScrollPane scrollPane = new JScrollPane(assetsTable);
+//        add(scrollPane, BorderLayout.CENTER);
+//
+//            updateDashboard();
+//    }
+//
+//    public void updateDashboard() {
+//        tableModel.setRowCount(0);
+//        String sql = "SELECT provider_name, SUM(amount) AS total_amount from assets GROUP BY provider_name ORDER BY provider_name";
+//
+//        double totalAssets = 0.0;
+//        try (Connection conn = DatabaseConnection.getConnection();
+//             PreparedStatement pstmt = conn.prepareStatement(sql);
+//             ResultSet rs = pstmt.executeQuery()) {
+//            while (rs.next()) {
+//                String provider = rs.getString("provider_name");
+//                double amount = rs.getDouble("total_amount");
+//                String type;
+//
+//                if (provider.equals("GCash") || provider.equals("Maya") || provider.equals("GrabPay") || provider.equals("ShopeePay")) {
+//                    type = "E-Wallet";
+//                } else {
+//                    type = "Bank";
+//                }
+//                tableModel.addRow(new Object[]{provider, type, String.format("₱%.2f", amount)});
+//                totalAssets += amount;
+//
+//                totalLabel.setText("Total Assets: " + String.format("₱%.2f", totalAssets));
+//            }
+//        } catch (Exception e) {
+//            JOptionPane.showMessageDialog(this, "Error loading dashboard: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+//        }
+//    }
 
-                totalLabel.setText("Total Assets: " + String.format("₱%.2f", totalAssets));
-            }
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(this, "Error loading dashboard: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-        }
     }
 }
